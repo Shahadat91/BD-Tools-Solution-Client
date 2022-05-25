46ef7b9e50f775e2e0b14dc9dd573b448927b96c
Shahadat91 <shahadat20000@gmail.com>
2022-05-26 02:11:08 +0600

    componets create

---
+++ src/components/SocialLogIn/SocialLogIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialLogIn = () => {
+    return (
+        <div>
+            <h2>This is Social Log In</h2>
+        </div>
+    );
+};
+
+export default SocialLogIn;
